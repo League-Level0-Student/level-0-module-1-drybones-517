35afdccfbdcf669643c75043389706df50e8edc8
hailinday <hailinday@gmail.com>
2024-03-09 12:48:34 -0800

    finished both of those

--- src/_03_if_else/_4_tasty_tomato/tasty_tomato/tasty_tomato.pde
+++ src/_03_if_else/_4_tasty_tomato/tasty_tomato/tasty_tomato.pde
@@ -4,14 +4,14 @@ void setup() {
 void draw() {
     background(200, 200, 200);
     noStroke();
-    fill(#CB4A4A);
+    fill(#E85B5B);
     ellipse(150, 200, 150, 150);
     ellipse(212,200,150,150);
     fill(#379B37);
     rect(176, 103, 12, 32);
     if(mousePressed){   
-      fill(#C9CBC9);
-    ellipse(80,200,25,25);
+      fill(#CBC9C9);
+    ellipse(75,200,25,25);
   }
     
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,35 +1,33 @@
 package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
+
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		int score = 0;
-		ask user"what has many keys but cant open a single lock";
-		correct answer "a piano";
-		if{
-			"correct answer a piano"
-			}
-		else{
-		score++;
+		String riddle = JOptionPane.showInputDialog(null, "what has many keys but cant open a single lock");
+
+		if (riddle.equals("piano")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "correct good job");
+		} else {
+			JOptionPane.showMessageDialog(null, "incorrect, the correct answer was a piano");
+
 		}
-		
-		// 1. make a variable to hold the score
 
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		String second = JOptionPane.showInputDialog(null, "what is black and white but read all over");
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		if (second.equals("newspaper")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "correct good job");
+		} else {
+			JOptionPane.showMessageDialog(null, "incorrect, the correct answer was a newspaper :/");
 
-		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
-		
+		}
 	}
 }
-
